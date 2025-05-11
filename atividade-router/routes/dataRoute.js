c39985da1e4f4aa74096bf1a3aade9e1185724b2
Pedro Barbosa <pedro.barbosa40@aluno.ifce.edu.br>
2025-05-11 20:57:35 -0300

    add atividade-router

--- atividade-router/routes/dataRoute.js
+++ atividade-router/routes/dataRoute.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
-  res.send(`Acesso à rota '/users' ${req.requestTime}`);
+  res.send(`Acesso à rota '/data' ${req.requestTime}`);
 });
 
 module.exports = router;
--- atividade-router/routes/usersSigninRoute.js
+++ atividade-router/routes/usersSigninRoute.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
-  res.send(`Acesso à rota '/users' ${req.requestTime}`);
+  res.redirect("http://localhost:3000/users/signup");
 });
 
 module.exports = router;
